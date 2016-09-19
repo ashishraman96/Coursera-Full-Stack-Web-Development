7a3e3b3702a22d2e7ee70c80f5be62335e659934
ashishraman96 <ashishraman81@gmail.com>
2016-09-20 00:05:58 +0530

    Web technology: Grunt

---
+++ Angular JS/conFusion/new.sh
@@ -0,0 +1,5 @@
+ sudo npm install grunt-contrib-concat --save-dev
+ sudo npm install grunt-contrib-cssmin --save-dev
+ sudo npm install grunt-contrib-uglify --save-dev
+ sudo npm install grunt-filerev --save-dev
+ sudo npm install grunt-usemin --save-dev
