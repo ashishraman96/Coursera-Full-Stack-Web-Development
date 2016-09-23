3179c6a6acafd18bc32759ba6da4ce628e3e9c1f
ashishraman96 <ashishraman81@gmail.com>
2016-09-23 21:07:19 +0530

    Fix minor bug

--- Angular JS/conFusion/app/scripts/app.js
+++ Angular JS/conFusion/app/scripts/app.js
@@ -94,8 +94,70 @@ var mod = angular.module('confusionApp', [])
                     console.log($scope.feedback);
                 }
             };
-        }]);
+        }])
+        
+        .controller('dishDetailController', ['$scope', function($scope) {
+
+            var dish={
+                          name:'Uthapizza',
+                          image: 'images/uthapizza.png',
+                          category: 'mains', 
+                          label:'Hot',
+                          price:'4.99',
+                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+                           comments: [
+                               {
+                                   rating:5,
+                                   comment:"Imagine all the eatables, living in conFusion!",
+                                   author:"John Lemon",
+                                   date:"2012-10-16T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:4,
+                                   comment:"Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                                   author:"Paul McVites",
+                                   date:"2014-09-05T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:3,
+                                   comment:"Eat it, just eat it!",
+                                   author:"Michael Jaikishan",
+                                   date:"2015-02-13T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:4,
+                                   comment:"Ultimate, Reaching for the stars!",
+                                   author:"Ringo Starry",
+                                   date:"2013-12-02T17:57:28.556094Z"
+                               },
+                               {
+                                   rating:2,
+                                   comment:"It's your birthday, we're gonna party!",
+                                   author:"25 Cent",
+                                   date:"2011-12-02T17:57:28.556094Z"
+                               }
+                               
+                           ]
+                    };
+            
+            $scope.dish = dish;
+            
+        }])
+      .controller('CommentController', ['$scope', function($scope){
+        $scope.comm={rating:"5", comment:"", author:"", date:""};
+        
+        $scope.postComment = function(){
+             $scope.comm.date = new Date().toISOString();
+            $scope.dish.comments.push($scope.comm);
+            $scope.commentForm.$setPristine();
+              $scope.comm={rating:"5", comment:"", author:"", date:""};
+            
+        }
+        
+    }]);
+        
+        ;
 
    
         
-        ;
+        
