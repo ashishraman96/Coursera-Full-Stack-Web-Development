9c4c04ee2d8622afee57da9f3eca90465a72710e
ashishraman96 <ashishraman81@gmail.com>
2017-02-10 12:02:04 +0530

    Splash Screen

--- Angular JS/conFusion/new.sh
+++
@@ -1,5 +0,0 @@
- sudo npm install grunt-contrib-concat --save-dev
- sudo npm install grunt-contrib-cssmin --save-dev
- sudo npm install grunt-contrib-uglify --save-dev
- sudo npm install grunt-filerev --save-dev
- sudo npm install grunt-usemin --save-dev
