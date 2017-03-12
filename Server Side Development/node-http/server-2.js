9d51959f4c17b787d0af14efa642d64c335d5a9b
ashishraman96 <ashishraman81@gmail.com>
2017-03-12 17:13:21 +0530

    REST API

--- Server Side Development/node-http/server-2.js
+++ Server Side Development/node-http/server-2.js
@@ -14,7 +14,8 @@ var server = http.createServer(function(req, res){
         var filePath = path.resolve('./public'+fileUrl);
         var fileExt = path.extname(filePath);
         if (fileExt == '.html') {
-       fs.exists(filePath, function(exists) {                 if (!exists) {
+       fs.exists(filePath, function(exists) {                 
+            if (!exists) {
         	res.writeHead(404, { 'Content-Type': 'text/html' });
         	res.end('<html><body><h1>Error 404: ' + fileUrl + 
                         ' not found</h1></body></html>');
